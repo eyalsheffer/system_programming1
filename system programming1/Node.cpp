a5d105a9f33c943a1b08a3b840b2294f1d11b0ac
eyalsheffer <eyal.sheffer05@gmail.com>
2025-04-06 15:11:52 +0300

    Delete system programming1 directory

--- system programming1/Node.cpp
+++
@@ -1,7 +0,0 @@
-#include "Node.hpp"
-
-// Constructor to initialize the node with a value
-Node::Node(int val) : data(val), next(nullptr) {}
-
-// Destructor (doesn't need to do anything special for this simple structure)
-Node::~Node() {}
